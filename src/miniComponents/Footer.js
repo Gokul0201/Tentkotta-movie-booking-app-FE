bb8924f1d28b58f7b97085fd6a685e02a481c64c
Gokul0201 <kgokula314@gmail.com>
2022-11-01 07:59:54 +0530

    commit

--- src/miniComponents/Footer.js
+++ src/miniComponents/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Footer() {
   return (
     <div className='footer'>
-        <h1>make<span>My</span>show </h1>
-       <p>Design & Built by Sairam</p>
-       <p>&copy; Sairam 2022</p>
+        <h1>Tentkotta</h1>
+       <p>Design & Built by Gokulakrishnan</p>
+       <p>&copy; Gokulakrishnan 2022</p>
     </div>
   )
 }
